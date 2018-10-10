49afd27a624ae4b875a918e3ef215cf4ed8dd7ec
Plamen <plamen.iliev@nevexis.com>
2018-10-10 22:38:41 +0300

    added changes to basic structure

--- src/main/java/com/goplaces/model/Place.java
+++ src/main/java/com/goplaces/model/Place.java
@@ -1,11 +1,11 @@
 package com.goplaces.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,16 +14,13 @@ import lombok.Setter;
 @Entity
 @Getter(value = AccessLevel.PUBLIC)
 @Setter(value = AccessLevel.PUBLIC)
-@NamedQueries({ // nl
-		// @NamedQuery(name = "getAllClients", query = "SELECT c FROM Client c") // nl
-})
 public class Place extends BaseModel {
 
 	@Column
 	private String name;
 
 	@Column
-	private String desc;
+	private String description;
 
 	@Column
 	private String category;
@@ -33,11 +30,11 @@ public class Place extends BaseModel {
 
 	@Column
 	private String latLng;
+
 	// @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
 	// property = "id")
 	// @JsonIdentityReference(alwaysAsId = true)
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinColumn(name = "user_id")
 	private User user;
-
 }
